6a7d231763286e4fd319960fa6a7481ae149f1df
chrisfinazzo <chris.finazzo@gmail.com>
2020-02-26 11:32:24 -0500

    Running JS inline is bad, move it to a file

---
+++ loadWorker.js
@@ -0,0 +1,8 @@
+   if (navigator.serviceWorker) {
+     navigator.serviceWorker.register('/sw.js')
+     .then( function (registration) {
+       console.log('Success!', registration.scope);
+     })
+     .catch( function (error) {
+       console.error('Failure!', error);
+}); }
