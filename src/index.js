fe8a3facdc476160f2d6f48e23cef8f85c0291cd
chrisfinazzo <chris.finazzo@gmail.com>
2020-10-07 09:48:58 -0400

    Add a service worker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import { render } from "react-snapshot";
 import Main from "./main";
 import "./index.scss";
+import * as serviceWorker from "./serviceWorker";
 
 render(
   <Main/>, 
   document.getElementById("root")
 );
+
+serviceWorker.register();
