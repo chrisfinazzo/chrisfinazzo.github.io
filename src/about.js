077d8d4ca053bb34bd3f4b99d453c5f2d563ef78
chrisfinazzo <chris.finazzo@gmail.com>
2020-10-15 23:25:35 -0400

    Add HashRouter and NavLinks

--- src/about.js
+++ src/about.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import {
+  NavLink,
+  HashRouter
+} from 'react-router-dom';
  
 class About extends Component {
   render() {
     return (
+      <HashRouter>
       <div>
-        <p className="intro">If you have a message to communicate, I can help you do that. Currently working as a Page at <a href="https://chathamlibrary.org">The Library of The Chathams</a>, I have an MA in Communications and am actively seeking a role which will allow me to leverage my unique mix of communications and technology skills. Take a look at my <Link to="/rjp">Reverse Job Posting</Link> to see how I can help you. I look forward to working <Link to="/contact">together</Link>.<br /><br />When I'm not working, I sometimes write things on my <a href="https://chrisfinazzo.com">blog</a>.</p>
+        <p className="intro">If you have a message to communicate, I can help you do that. Currently working as a Page at <a href="https://chathamlibrary.org">The Library of The Chathams</a>, I have an MA in Communications and am actively seeking a role which will allow me to leverage my unique mix of communications and technology skills. Take a look at my <NavLink to="/rjp">Reverse Job Posting</NavLink> to see how I can help you. I look forward to working <NavLink to="/contact">together</NavLink>.<br /><br />When I'm not working, I sometimes write things on my <a href="https://chrisfinazzo.com">blog</a>.</p>
       </div>
+    </HashRouter>
     );
   }
 }
