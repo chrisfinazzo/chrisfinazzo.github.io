7e7bd803afdc548387e798edec6ecda9b1977025
chrisfinazzo <chris.finazzo@gmail.com>
2020-10-26 10:11:28 -0400

    Add icons, refactor

--- src/about.js
+++ src/about.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import {
   NavLink,
   HashRouter
 } from 'react-router-dom';
+import me from './me.jpeg';
  
 class About extends Component {
   render() {
     return (
       <HashRouter>
-      <div>
-        <p className="intro">If you have a message to communicate, I can help you do that. Currently working as a Page at <a href="https://chathamlibrary.org">The Library of The Chathams</a>, I have an MA in Communications and am actively seeking a role which will allow me to leverage my unique mix of communications and technology skills. Take a look at my <NavLink to="/rjp">Reverse Job Posting</NavLink> to see how I can help you. I look forward to working <NavLink to="/contact">together</NavLink>.<br /><br />When I'm not working, I sometimes write things on my <a href="https://chrisfinazzo.com">blog</a>.</p>
+      <div id="nameplate">
+      <h2>Chris Finazzo</h2>
+      <p>One part writer, one part technologist</p>
       </div>
+      <div className="content">
+      <img src={me} alt="me" />
+      <p className="intro">If you have a message to communicate, I can help you do that. Currently working as a Page at <a href="https://chathamlibrary.org">The Library of The Chathams</a>, I have an MA in Communications and am actively seeking a role which will allow me to leverage my unique mix of communications and technology skills. Take a look at my <NavLink to="/rjp">Reverse Job Posting</NavLink> to see how I can help you. I look forward to working <NavLink to="/contact">together</NavLink>.<br /><br />When I'm not working, I sometimes write things on my <a href="https://chrisfinazzo.com">blog</a>.</p>
+      </div>
+
+<div id="social">
+<FontAwesomeIcon icon={faGithub} size="2x" />
+<FontAwesomeIcon icon={faLinkedin} size="2x" />
+<FontAwesomeIcon icon={faTwitter} size="2x" />
+</div>
+
+      <p id="rights">&copy; 2020 Chris Finazzo</p>
     </HashRouter>
     );
   }
