e0402da10a4d1fa7be1bbc8ea3bcb67e76476967
chrisfinazzo <chris.finazzo@gmail.com>
2020-10-29 10:04:19 -0400

    Move to React Snap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { render } from "react-snapshot";
+import { hydrate, render } from "react-dom";
 import App from "./App"
 import * as serviceWorker from "./serviceWorker";
 
-render(
-  <App/>, 
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
 serviceWorker.register();
