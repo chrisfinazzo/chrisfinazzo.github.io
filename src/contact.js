dcc1cc56511d134cb0423e7d85e2b68fde834795
chrisfinazzo <chris.finazzo@gmail.com>
2020-11-11 10:40:17 -0500

    Fix label htmlFor tag

--- src/contact.js
+++ src/contact.js
@@ -16,7 +16,7 @@ class Contact extends Component {
   </div>
   <label htmlFor="fname">First Name</label><input type="text" className="field" name="firstname" placeholder="John" />
 <label htmlFor="lname">Last Name</label><input type="text" className="field" name="lastname" placeholder="Smith" />
-<label htmlFor="email">Email</label><input type="text" className="field" name="_replyto" placeholder="john.smith@example.com" /><label for="message">Message</label>
+<label htmlFor="email">Email</label><input type="text" className="field" name="_replyto" placeholder="john.smith@example.com" /><label htmlFor="message">Message</label>
     <textarea className="field" name="message" placeholder=""></textarea>
 <input type="submit" name="submit" value="Submit" />
 </form>
