11bebf912f4dcd23656746c1a96726a5bd33030b
chrisfinazzo <chris.finazzo@gmail.com>
2021-03-16 15:58:58 -0400

    Register new Service Worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { hydrate, render } from "react-dom";
 import App from "./App"
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorkerRegistration'
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
