8e946e1b206988485989d30222cbc56023ad27cc
chrisfinazzo <chris.finazzo@gmail.com>
2021-04-06 17:14:02 -0400

    Code splitting all the way down

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Lazy, Suspense } from "react";
 import {
   Route,
   HashRouter
 } from "react-router-dom";
-import Home from "./home";
-import RJP from "./rjp";
-import Contact from "./contact";
+const Home = React.lazy(() => import("./home"));
+const RJP = React.lazy(() => import("./rjp"));
+const Contact = React.lazy(() => import("./contact"));
 import Header from "./components/header";
 import Social from "./components/social";
 import Rights from "./components/rights";
@@ -13,11 +13,13 @@ import Rights from "./components/rights";
 class App extends Component {
   render() {
     return (
+      <React.Suspense fallback={<p>Loading...</p>}>
       <HashRouter>
             <Route exact path="/" component={Home}/>
             <Route path="/rjp" component={RJP}/>
             <Route path="/contact" component={Contact}/>
       </HashRouter>
+      </React.Suspense>
     );
   }
 }
