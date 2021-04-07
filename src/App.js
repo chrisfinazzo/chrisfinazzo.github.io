a4b0b989a62ee855300b8288e176544d05f6047b
chrisfinazzo <chris.finazzo@gmail.com>
2021-04-07 11:01:26 -0400

    Tweak code splitting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Lazy, Suspense } from "react";
+import React, { Component, lazy, Suspense } from "react";
 import {
   Route,
   HashRouter
@@ -6,6 +6,9 @@ import {
 const Home = React.lazy(() => import("./home"));
 const RJP = React.lazy(() => import("./rjp"));
 const Contact = React.lazy(() => import("./contact"));
+
+const renderLoader = () => <p>Loading...</p>;
+
 import Header from "./components/header";
 import Social from "./components/social";
 import Rights from "./components/rights";
@@ -13,13 +16,13 @@ import Rights from "./components/rights";
 class App extends Component {
   render() {
     return (
-      <React.Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={renderLoader()}>
       <HashRouter>
             <Route exact path="/" component={Home}/>
             <Route path="/rjp" component={RJP}/>
             <Route path="/contact" component={Contact}/>
       </HashRouter>
-      </React.Suspense>
+      </Suspense>
     );
   }
 }
