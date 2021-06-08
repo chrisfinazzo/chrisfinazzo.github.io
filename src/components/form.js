2ed254c7eea8164d87bec7395534ffc9d2ed844e
chrisfinazzo <chris.finazzo@gmail.com>
2021-06-08 12:01:16 -0400

    Add placeholder data and required attributes

--- src/components/form.js
+++ src/components/form.js
@@ -6,22 +6,27 @@ const Form = () => (
   <h2 id="contact">Contact Me</h2>
   <div>
     <label for="name">Name:&nbsp;</label>
-    <input type="text" id="name" name="user_name" />
+    <input type="text" id="name" name="user_name" placeholder="John Smith" required />
   </div>
 
   <div>
     <label for="mail">E-mail:&nbsp;</label>
-    <input type="email" id="mail" name="user_email" />
+    <input type="email" id="mail" name="user_email" placeholder="john.smith@example.com" required />
   </div>
 
   <div>
     <label for="msg">Message:&nbsp;</label>
-    <textarea id="msg" name="user_message"></textarea>
+    <textarea id="msg" name="user_message" required></textarea>
   </div>
 
   <div class="button">
     <button type="submit">Submit</button>
   </div>
+  
+  <div>
+    <label><input name="bot-field" hidden /></label>
+  </div>
+  
 </form>
 )
 
